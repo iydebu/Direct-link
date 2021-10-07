89f3ebf1d0aeeab2d90b11c70e2ea0fb89d25d50
Devashish Tiwari <91595411+iydebu@users.noreply.github.com>
2021-10-07 21:46:19 +0530

    Create direct_link.c

---
+++ direct_link.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <conio.h>
+#endif
+void logo()
+{
+    printf("    ____  _                __     ___       __  \n");
+    printf("   / __ \\(_)_______  _____/ /_   / (_)___  / /__ \n");
+    printf("  / / / / / ___/ _ \\/ ___/ __/  / / / __ \\/ //_/ \n");
+    printf(" / /_/ / / /  /  __/ /__/ /_   / / / / / / , <    \n");
+    printf("/_____/_/_/   \\___/\\___/\\__/  /_/_/_/ /_/_/|_|   \n\n");
+}
+
+#ifdef _WIN32
+int clip(const char *output)
+{
+    const size_t len = strlen(output) + 1;
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), output, len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+    return 1;
+}
+#endif
+
+int main()
+{
+    int i = 0;
+    char *link = (char *)malloc(sizeof(char) * 100);
+    char *id = (char *)malloc(sizeof(char) * 100);
+    logo();
+    printf("\nEnter the link: ");
+    fgets(link, 100, stdin);
+    char *index = strstr(link, "d/");
+    if (index == NULL)
+    {
+        printf("\ninvalid link\n");
+        return 0;
+    }
+    index += 2;
+    for (i = 0; index[i] != '/' && i <= 100; i++)
+        ;
+    if (i >= 100)
+    {
+        printf("\ninvalid link\n");
+        return 0;
+    }
+    index[i] = '\0';
+    strcpy(id, index);
+    strcpy(link, "https://drive.google.com/uc?export=download&id=");
+    strcat(link, id);
+    printf("\nYour link:- %s\n", link);
+    (clip(link) == 1) ? printf("\nCopied succesfully...\n") : printf("\nfailed to copy!\n");
+    printf("\n*****DEBU******\n");
+#ifdef _WIN32
+    system("cls");
+#else
+    getch();
+#endif
+}
